7cce1e152dd85b14d0d5dab70dfa26a19c87a0a1
Umarov Ismoiljon <hs.umarov21@gmail.com>
2025-03-06 11:16:37 +0500

    cleanup; stubs

--- database/migrations/2018_08_08_100000_create_telescope_entries_table.php
+++ database/migrations/2018_08_08_100000_create_telescope_entries_table.php
@@ -4,19 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Get the migration connection name.
-     */
+return new class extends Migration {
     public function getConnection(): ?string
     {
         return config('telescope.storage.database.connection');
     }
 
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         $schema = Schema::connection($this->getConnection());
@@ -56,9 +49,6 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         $schema = Schema::connection($this->getConnection());
