f257edbb428595c3d14043f5ba6a5cd93b570ab7
Umarov Ismoiljon <hs.umarov21@gmail.com>
2025-03-06 09:32:27 +0500

    session_tokens

--- database/migrations/2025_03_06_043049_create_session_tokens_table.php
+++ database/migrations/2025_03_06_043049_create_session_tokens_table.php
@@ -4,22 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('session_tokens', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('device')->nullable();
+            $table->string('ip')->nullable();
+            $table->text('token');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('session_tokens');
