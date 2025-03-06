b5f51a2186c7eccf2f9ad742194a89b500300f8d
Umarov Ismoiljon <hs.umarov21@gmail.com>
2025-03-06 09:45:23 +0500

    modify token column type in personal_access_tokens

---
+++ database/migrations/2025_03_06_044403_modify_token_column_in_personal_access_tokens.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('personal_access_tokens', function (Blueprint $table) {
+            $table->text('token')->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('personal_access_tokens', function (Blueprint $table) {
+            $table->string('token', 255)->change();
+        });
+    }
+};
